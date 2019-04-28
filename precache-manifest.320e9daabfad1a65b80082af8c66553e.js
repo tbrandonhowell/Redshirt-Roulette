c932a664bcc50fa7dff8d9728e87bcd9c2ef847e
Brandon H <45020672+tbrandonhowell@users.noreply.github.com>
2019-04-28 00:43:46 -0400

    Updates

---
+++ precache-manifest.320e9daabfad1a65b80082af8c66553e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88fcbd1f1bbfda566ccb4986d59935c9",
+    "url": "/Redshirt-Roulette/index.html"
+  },
+  {
+    "revision": "0b01a94e09a52aa6fdce",
+    "url": "/Redshirt-Roulette/static/css/main.969e09f5.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/Redshirt-Roulette/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "0b01a94e09a52aa6fdce",
+    "url": "/Redshirt-Roulette/static/js/main.ba824dca.chunk.js"
+  },
+  {
+    "revision": "a545380b1fb4e110f16e",
+    "url": "/Redshirt-Roulette/static/js/runtime~main.75bc040e.js"
+  }
+]);
